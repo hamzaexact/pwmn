81e2e66fa0836b14e6fa02f956ad98fcbc3c8a61
Hamza <official.hamzaderaoui@gmail.com>
2025-11-20 17:58:52 +0100

    fetch name of the token by the by the token it self

--- src/interpreter/mod.rs
+++ src/interpreter/mod.rs
@@ -2,18 +2,69 @@ pub mod ast;
 pub mod lexer;
 pub mod parser;
 pub use crate::error::LexerErr;
+use lexer::TokenKind;
 
-pub use lexer::{Span,Token};
+pub use lexer::{Span, Token};
 
+pub fn push_token(collector: &mut Vec<Token>, tTokind: lexer::TokenKind, start: usize, end: usize) {
+    collector.push(Token {
+        kind: tTokind,
+        span: Span { start, end },
+    });
+}
 
-pub fn push_token(collector: &mut Vec<Token> ,tTokind: lexer::TokenKind, start:usize, end:usize) {
-    collector.push(
-        Token {
-            kind: tTokind,
-            span: Span {
-                start,
-                end
-            }
-        }
-    );
+pub fn token_name(tokind: &TokenKind) -> &str {
+    match tokind {
+        TokenKind::Add => "Add",
+        TokenKind::As => "As",
+        TokenKind::Audit => "Audit",
+        TokenKind::Connect => "Connect",
+        TokenKind::Create => "Create",
+        TokenKind::Contains => "Contains",
+        TokenKind::Delete => "Delete",
+        TokenKind::Drop => "Drop",
+        TokenKind::Describe => "Describe",
+        TokenKind::Destroy => "Destroy",
+        TokenKind::Disable => "Disable",
+        TokenKind::Disconnect => "Disconnect",
+        TokenKind::Enable => "Enable",
+        TokenKind::From => "From",
+        TokenKind::Generate => "Generate",
+        TokenKind::Generated => "Generated",
+        TokenKind::Init => "Init",
+        TokenKind::Insert => "Insert",
+        TokenKind::Into => "Into",
+        TokenKind::List => "List",
+        TokenKind::Limit => "Limit",
+        TokenKind::Metadata => "Metadata",
+        TokenKind::Password => "Password",
+        TokenKind::Prompt => "Prompt",
+        TokenKind::Register => "Register",
+        TokenKind::Rotate => "Rotate",
+        TokenKind::Set => "Set",
+        TokenKind::Select => "Select",
+        TokenKind::Status => "Status",
+        TokenKind::Update => "Update",
+        TokenKind::Where => "Where",
+        TokenKind::With => "With",
+        TokenKind::Bool(_) => "Bool",
+        TokenKind::Identifier(_) => "Identifier",
+        TokenKind::String(_) => "String",
+        TokenKind::Number(_) => "Number",
+        TokenKind::Semicolon => "Semicolon",
+        TokenKind::Log => "Log",
+        TokenKind::And => "And",
+        TokenKind::Comma => "Comma",
+        TokenKind::Or => "Or",
+        TokenKind::Ge => "Ge",
+        TokenKind::Gt => "Gt",
+        TokenKind::Le => "Le",
+        TokenKind::Lt => "Lt",
+        TokenKind::To => "To",
+        TokenKind::Equals => "Equals",
+        TokenKind::NotEquals => "NotEquals",
+        TokenKind::LeftParen => "LeftParen",
+        TokenKind::RightParen => "RightParen",
+        TokenKind::Astrisk => "Astrisk",
+    }
 }
