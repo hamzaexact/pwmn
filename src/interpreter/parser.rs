61118a05b703561cb53dbe4e24b6a8b13c332048
Hamza <official.hamzaderaoui@gmail.com>
2025-11-20 17:57:56 +0100

    add the ability to peek and consume with proper error handling

--- src/interpreter/parser.rs
+++ src/interpreter/parser.rs
@@ -1,39 +1,62 @@
-// use crate::interpreter::ast;
+use crate::error::ParserErr;
+use crate::interpreter::lexer::{LexResult, Span, Token, TokenKind};
+use crate::interpreter::{ast, lexer};
 //
-// use crate::interpreter::lexer::Tokens;
-//
-// pub enum ParseErr {}
-//
-// pub struct Parser<'t> {
-//     tokens: Vec<Tokens>,
-//     tTokens: std::iter::Peekable<std::slice::Iter<'t, Tokens>>,
-//     idx: usize,
-// }
-// impl<'t> Parser<'t> {
-//     pub fn parse(tokens: Vec<Tokens>) -> Result<ast::Stmt, ParseErr> {
-//         let mut pP = Parser {
-//             tokens: tokens.clone(),
-//             tTokens: tokens.iter().peekable(),
-//             idx: 0,
-//         };
-//         match pP.parse_stmt() {
-//             Ok(stmt) => Ok(stmt),
-//             Err(e) => return Err(e),
-//         }
-//     }
-//
-//     fn parse_stmt(&mut self) -> Result<ast::Stmt, ParseErr> {
-//         let mut tokens = self.tokens.iter().peekable();
-//         let expr = self.depth1();
-//         Ok(ast::Stmt::Empty)
-//     }
-//
-//     fn depth1(&mut self) -> Result<ast::Stmt, ParseErr> {
-//         let expr = self.depth1()?;
-//         Ok(ast::Stmt::Empty)
-//     }
-//
-//     fn root(&mut self) -> Result<ast::Stmt, ParseErr> {
-//         Ok(ast::Stmt::Empty)
-//     }
-// }
+pub struct Parser<'t> {
+    query: &'t str,
+    tokens: Vec<Token>,
+    pos: usize,
+}
+
+impl<'t> Parser<'t> {
+    pub fn parse(lex_res: LexResult) -> Result<ast::Stmt, ParserErr> {
+        let mut parser = Parser {
+            query: lex_res.query,
+            tokens: lex_res.tokens,
+            pos: 0,
+        };
+        let k = parser.consume(TokenKind::Select);
+        let f = parser.consume(TokenKind::As);
+        match f {
+            Ok(_) => return Ok(ast::Stmt::Empty),
+            Err(e) => return Err(e),
+        }
+        Ok(ast::Stmt::Empty)
+    }
+
+    fn peek_token(&self) -> Option<&Token> {
+        if self.pos < self.tokens.len() {
+            return Some(&self.tokens[self.pos]);
+        }
+        None
+    }
+
+    fn consume(&mut self, expected_token: TokenKind) -> Result<(), ParserErr> {
+        let token = self.peek_token();
+        match token {
+            Some(tk) => {
+                if (*tk).kind == expected_token {
+                    self.pos += 1;
+                } else {
+                    return Err(ParserErr::TypeMismatch {
+                        input: self.query.to_string(),
+                        expectedkind: expected_token,
+                        givenkind: tk.kind.clone(),
+                        span: token.unwrap().span.clone(),
+                    });
+                }
+            }
+            None => {
+                return Err(ParserErr::UnexpectedEndOfExpression {
+                    input: self.query.to_string(),
+                    tokind: expected_token,
+                    span: Span {
+                        start: self.pos,
+                        end: self.pos + 1,
+                    },
+                });
+            }
+        }
+        Ok(())
+    }
+}
