2f09257f0480c22707d415cb801f429d9bbf2f04
Hamza <official.hamzaderaoui@gmail.com>
2025-11-24 19:45:30 +0100

    Fix the wrong creating of rvault.bin file inside the main src dir

--- src/storage/init.rs
+++ src/storage/init.rs
@@ -17,8 +17,6 @@ pub fn init() -> Result<(), Box<dyn std::error::Error>> {
         return Err(Box::new(InitErr::VaultAlreadyExists));
     }
     mksafe_dir(&root_folder)?;
-    let vault_path = root_folder.join(FNAME);
-    OO::new().read(true).write(true).create(true).open(FNAME)?;
     RV::new()?;
     let s_msg = format!(
         "initialzed empty vault repository in {}/{}",
