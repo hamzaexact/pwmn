c54d879769e6172c4f52b3ab2e4b38d742def981
Hamza <official.hamzaderaoui@gmail.com>
2025-11-27 04:06:54 +0100

    Update eval function to require session connection.

--- src/interpreter/eval.rs
+++ src/interpreter/eval.rs
@@ -1,15 +1,17 @@
 use crate::interpreter::ast::{self, Stmt};
-use crate::statements::create;
+use crate::session::offSessionConn::SessionConn;
+use crate::statements::{create};
 use crate::storage::init;
 pub trait eval {
-    fn eval(&self) -> Result<(), Box<dyn std::error::Error>>;
+    fn eval(&self, session: &mut SessionConn) -> Result<(), Box<dyn std::error::Error>>;
 }
 
 impl eval for Stmt {
-    fn eval(&self) -> Result<(), Box<dyn std::error::Error>> {
+    fn eval(&self, session: &mut SessionConn) -> Result<(), Box<dyn std::error::Error>> {
         match self {
             Self::Init => init::init()?,
-            Self::Create { reg_name } => create::CreateRegExec::execute(reg_name)?,
+            Self::Create { reg_name } => create::CreateRegExec::execute(reg_name, session)?,
+            Self::Create { reg_name } => todo!(),
             other => {
                 println!("TODO -> {:?}", other);
             }
