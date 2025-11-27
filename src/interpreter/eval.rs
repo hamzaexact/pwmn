7be927dd3871bae3855805e5aea2eca46e7021f0
Hamza <official.hamzaderaoui@gmail.com>
2025-11-27 05:36:43 +0100

    Update eval trait to use connect and create methods instead of todo!

--- src/interpreter/eval.rs
+++ src/interpreter/eval.rs
@@ -1,6 +1,6 @@
 use crate::interpreter::ast::{self, Stmt};
 use crate::session::offSessionConn::SessionConn;
-use crate::statements::{create};
+use crate::statements::{connect, create};
 use crate::storage::init;
 pub trait eval {
     fn eval(&self, session: &mut SessionConn) -> Result<(), Box<dyn std::error::Error>>;
@@ -11,7 +11,7 @@ impl eval for Stmt {
         match self {
             Self::Init => init::init()?,
             Self::Create { reg_name } => create::CreateRegExec::execute(reg_name, session)?,
-            Self::Create { reg_name } => todo!(),
+            Self::Connect { reg_name } => connect::VaultConnection::execute(reg_name, session)?,
             other => {
                 println!("TODO -> {:?}", other);
             }
